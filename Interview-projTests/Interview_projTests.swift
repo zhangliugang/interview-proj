6bb8b60bae82d43240efd96570c9f348c0d70489
August <zhangliugang@gmail.com>
2020-11-17 14:26:08 +0800

    init proj

---
+++ Interview-projTests/Interview_projTests.swift
@@ -0,0 +1,33 @@
+//
+//  Interview_projTests.swift
+//  Interview-projTests
+//
+//  Created by August on 11/17/20.
+//
+
+import XCTest
+@testable import Interview_proj
+
+class Interview_projTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
